1b1999fef84f2bd5bb09ca156c4d17de9b06a56c
vianwu33116 <101929742+vianwu33116@users.noreply.github.com>
2024-01-30 07:49:55 +0000

    [WIP] bookingSuccess page finish remain integrate with roomBooking page

--- src/app/bookingSuccess/page.tsx
+++ src/app/bookingSuccess/page.tsx
@@ -1,27 +1,102 @@
 'use client';
 
 import * as React from 'react';
-import { Box, Button, Container, Grid, Stack, Typography, Link } from '@mui/material';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-//import { useMediaQuery } from '@mui/material';
 import Check from '@mui/icons-material/Check';
-import SquareCard from '@/components/room/SquareCard';
 import type { NextPage } from 'next';
 import Card from '@/components/common/Card';
 import { useWidth } from '@/hooks';
 import Headline from '@/components/common/Headline';
-import RoomFacilityBlock from '@/components/room/RoomFacilityBlock';
 
-const facilityInfo = [
-  {
-    title: '平面電視',
-    isProvide: true,
+
+const orderInfo = {
+  status: true,
+  result: {
+    userInfo: {
+      address: {
+        zipcode: 802,
+        detail: '文山路23號',
+      },
+      name: 'Joanne Chen',
+      phone: '0912345678',
+      email: 'example@gmail.com',
+    },
+    _id: '653e335a13831c2ac8c389bb',
+    roomId: {
+      name: '尊爵雙人房',
+      description: '享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。',
+      imageUrl: 'https://fakeimg.pl/300/',
+      imageUrlList: ['https://fakeimg.pl/300/', 'https://fakeimg.pl/300/', 'https://fakeimg.pl/300/'],
+      areaInfo: '24坪',
+      bedInfo: '一張大床',
+      maxPeople: 4,
+      price: 10000,
+      status: 1,
+      layoutInfo: [
+        {
+          title: '市景',
+          isProvide: true,
+        },
+      ],
+      facilityInfo: [
+        {
+          title: '平面電視',
+          isProvide: true,
+        },
+        {
+          title: '吹風機',
+          isProvide: true,
+        },
+        {
+          title: '手電筒',
+          isProvide: true,
+        },
+        {
+          title: '音響',
+          isProvide: true,
+        },
+      ],
+      amenityInfo: [
+        {
+          title: '衛生紙',
+          isProvide: true,
+        },
+        {
+          title: '拖鞋',
+          isProvide: true,
+        },
+        {
+          title: '牙刷',
+          isProvide: true,
+        },
+      ],
+      _id: '653e4661336cdccc752127a0',
+      createdAt: '2023-10-29T11:47:45.641Z',
+      updatedAt: '2023-10-29T11:47:45.641Z',
+    },
+    checkInDate: '2024-01-03T16:00:00.000Z',
+    checkOutDate: '2024-01-04T16:00:00.000Z',
+    peopleNum: 2,
+    orderUserId: '6533f0ef4cdf5b7f762747b0',
+    status: 0,
+    createdAt: '2024-10-29T10:26:34.498Z',
+    updatedAt: '2023-10-29T10:26:34.498Z',
   },
-  {
-    title: '吹風機',
-    isProvide: true,
-  },
-];
+};
+
+function getDay(date: string): string {
+  const days = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+  const num = new Date(date).getDay();
+  return days[num];
+}
+
+function timeFormat(checkDate: string): string {
+  const month = checkDate.split('-')[1];
+  const day = checkDate.split('-')[2].split('T')[0];
+  const date = checkDate.split('T')[0];
+  return `${month} 月 ${day} 號${getDay(date)}`;
+}
 
 const BookingSuccess: NextPage = () => {
   const theme = useTheme();
@@ -32,10 +107,8 @@ const BookingSuccess: NextPage = () => {
     <>
       <Box sx={{ color: '#ffffff' }}>
         <Container>
-          <Stack
-            direction={{ md: 'column', lg: 'row' }}
-            justifyContent={{ md: 'center', lg: 'space-between' }}>
-            <Box width={{ md: '100%', lg: '55%'}} sx={{ maxWidth: '746px'}}>
+          <Stack direction={{ md: 'column', lg: 'row' }} justifyContent={{ md: 'center', lg: 'space-between' }}>
+            <Box width={{ md: '100%', lg: '55%' }} sx={{ maxWidth: '746px' }}>
               <Box
                 component="section"
                 mb={{ sm: '40px', md: '80px' }}
@@ -57,16 +130,18 @@ const BookingSuccess: NextPage = () => {
                     mb={{ sm: 2, md: 0 }}>
                     <Check sx={{ fontSize: 40, color: '#ffffff' }} />
                   </Stack>
-                  <Typography component="h2" fontSize={{ sm: '32px', md: '40px'}} fontWeight={700}>
+                  <Typography component="h2" fontSize={{ sm: '32px', md: '40px' }} fontWeight={700}>
                     恭喜，Jessica！
                     <br />
                     您已預訂成功
                   </Typography>
                 </Stack>
-                <Typography width={'88%'}>我們已發送訂房資訊及詳細內容至你的電子信箱，入住時需向櫃檯人員出示訂房人證件。</Typography>
+                <Typography width={'88%'}>
+                  我們已發送訂房資訊及詳細內容至你的電子信箱，入住時需向櫃檯人員出示訂房人證件。
+                </Typography>
               </Box>
               <Box component="section" mb={{ sm: 5, md: 10 }} pb={{ sm: 5, md: 10 }} borderBottom={'1px solid #ececec'}>
-                <Typography component="h3" fontSize={{ sm: '16px', md: '24px'}} fontWeight={700} mb={{ sm: 3, md: 5 }}>
+                <Typography component="h3" fontSize={{ sm: '16px', md: '24px' }} fontWeight={700} mb={{ sm: 3, md: 5 }}>
                   立即查看您的訂單紀錄
                 </Typography>
                 <Button variant="contained">前往我的訂單</Button>
@@ -76,15 +151,15 @@ const BookingSuccess: NextPage = () => {
                   訂房人資訊
                 </Typography>
                 <Typography mb={1}>姓名</Typography>
-                <Typography mb={3}>Jessica Ｗang</Typography>
+                <Typography mb={3}>{orderInfo.result.userInfo.name}</Typography>
                 <Typography mb={1}>手機號碼</Typography>
-                <Typography mb={3}>+886 912 345 678</Typography>
+                <Typography mb={3}>{orderInfo.result.userInfo.phone}</Typography>
                 <Typography mb={1}>電子信箱</Typography>
-                <Typography mb={7}>jessica@sample.com</Typography>
+                <Typography mb={7}>{orderInfo.result.userInfo.email}</Typography>
               </Box>
             </Box>
             <Box
-              width={{ md: '100%', lg: '38%'}}
+              width={{ md: '100%', lg: '38%' }}
               sx={{
                 maxWidth: '478px',
               }}>
@@ -93,10 +168,14 @@ const BookingSuccess: NextPage = () => {
                 sx={{
                   display: 'flex',
                   flexDirection: 'column',
-                  marginBottom: '60px'
+                  marginBottom: '60px',
                 }}>
-                <Typography mb={1}>預訂參考編號： HH2302183151222</Typography>
-                <Typography component="h4" mb={{ sm: 3, md: 5 }} fontSize={{ sm: '1rem', md: '1.5rem'}} fontWeight={700}>
+                <Typography mb={1}>預訂參考編號： {orderInfo.result.orderUserId}</Typography>
+                <Typography
+                  component="h4"
+                  mb={{ sm: 3, md: 5 }}
+                  fontSize={{ sm: '1rem', md: '1.5rem' }}
+                  fontWeight={700}>
                   即將來的行程
                 </Typography>
                 <img
@@ -106,46 +185,64 @@ const BookingSuccess: NextPage = () => {
                 />
                 <Box mb={{ sm: 3, md: 5 }} pb={{ sm: 3, md: 5 }} borderBottom={'1px solid #ECECEC'}>
                   <Stack direction={'row'} mb={3}>
-                    <Typography fontSize={{ sm: '16px', md: '20px'}} fontWeight={700}>
-                      尊爵雙人房，1 晚&nbsp;&nbsp;
+                    <Typography fontSize={{ sm: '16px', md: '20px' }} fontWeight={700}>
+                      {orderInfo.result.roomId.name}&nbsp;&nbsp;
                     </Typography>
-                    <Typography color={'#909090'} fontSize={{ sm: '16px', md: '20px'}} fontWeight={700}>
+                    <Typography color={'#909090'} fontSize={{ sm: '16px', md: '20px' }} fontWeight={700}>
                       |
                     </Typography>
-                    <Typography fontSize={{ sm: '16px', md: '20px'}} fontWeight={700}>
-                      &nbsp;&nbsp;住宿人數：2 位
+                    <Typography fontSize={{ sm: '16px', md: '20px' }} fontWeight={700}>
+                      &nbsp;&nbsp;住宿人數：{orderInfo.result.peopleNum} 位
                     </Typography>
                   </Stack>
                   <Box mb={3}>
-                    <Typography fontWeight={700}>入住：6 月 10 日星期二，15:00 可入住</Typography>
-                    <Typography fontWeight={700}>退房：6 月 11 日星期三，12:00 前退房</Typography>
+                    <Typography fontWeight={700}>
+                      入住：{timeFormat(orderInfo.result.checkInDate)}，15:00 可入住
+                    </Typography>
+                    <Typography fontWeight={700}>
+                      退房：{timeFormat(orderInfo.result.checkOutDate)}，12:00 前退房
+                    </Typography>
                   </Box>
-                  <Typography fontWeight={700}>NT$ 10,000</Typography>
+                  <Typography fontWeight={700}>NT$ {orderInfo.result.roomId.price}</Typography>
                 </Box>
                 <Box sx={{ mb: 3 }}>
                   <Headline title="房內設備" />
-                  <Stack direction={'row'} mt={3} p={3} border={'1px solid #ececec'} borderRadius={1}>
-                    <Box sx={{ display: 'flex' }} mr={'40px'}>
-                      <Check color="primary" sx={{ fontSize: 24 }} />
-                      <Typography>平面電視</Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex' }} mr={'40px'}>
-                      <Check color="primary" sx={{ fontSize: 24 }} />
-                      <Typography>吹風機</Typography>
-                    </Box>
+                  <Stack
+                    direction={'row'}
+                    mt={3}
+                    p={3}
+                    border={'1px solid #ececec'}
+                    borderRadius={1}
+                    useFlexGap
+                    flexWrap="wrap">
+                    {orderInfo.result.roomId.facilityInfo.map((item) => {
+                      return (
+                        <Box sx={{ display: 'flex' }} mb={'10px'} width={'50%'}>
+                          <Check color="primary" sx={{ fontSize: 24 }} />
+                          <Typography>{item.title}</Typography>
+                        </Box>
+                      );
+                    })}
                   </Stack>
                 </Box>
                 <Box sx={{ mb: 3 }}>
                   <Headline title="備品提供" />
-                  <Stack direction={'row'} mt={3} p={3} border={'1px solid #ececec'} borderRadius={1}>
-                    <Box sx={{ display: 'flex' }} mr={'40px'}>
-                      <Check color="primary" sx={{ fontSize: 24 }} />
-                      <Typography>衛生紙</Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex' }} mr={'40px'}>
-                      <Check color="primary" sx={{ fontSize: 24 }} />
-                      <Typography>拖鞋</Typography>
-                    </Box>
+                  <Stack
+                    direction={'row'}
+                    mt={3}
+                    p={3}
+                    border={'1px solid #ececec'}
+                    borderRadius={1}
+                    useFlexGap
+                    flexWrap="wrap">
+                    {orderInfo.result.roomId.amenityInfo.map((item) => {
+                      return (
+                        <Box sx={{ display: 'flex' }} mb={'10px'} width={'50%'}>
+                          <Check color="primary" sx={{ fontSize: 24 }} />
+                          <Typography>{item.title}</Typography>
+                        </Box>
+                      );
+                    })}
                   </Stack>
                 </Box>
               </Card>
